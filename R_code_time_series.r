24163b778e10d82cf11f42586a5b96201adc1530
Flavia Ruggirello <80512151+FlaviaRuggirello@users.noreply.github.com>
2021-03-31 13:00:22 +0200

    Create R_code_time_series.r

---
+++ R_code_time_series.r
@@ -0,0 +1,10 @@
+#Time series analysis
+#Greenland increase temperature
+#Data anc code from Emanuela 
+
+setwd("~/Desktop/lab/greenland")
+
+#che pacchetti utilizzeremo???
+
+# install.packages("raster")
+library(raster)
