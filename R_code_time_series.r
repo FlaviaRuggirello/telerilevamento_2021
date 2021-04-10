fb7a947ee80864cd94a3f9514ba6cac448ff36eb
Flavia Ruggirello <80512151+FlaviaRuggirello@users.noreply.github.com>
2021-04-10 18:56:44 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -77,4 +77,28 @@ plotRGB(TGr, 1, 2, 3, stretch="Lin")
 
 #coloristRpackage -> ne parlerà
 
+#VENERDI' 9 SALTATO LEZIONE - MANCANO MIEI COMMENTI !!!!
 
+levelplot(TGr)
+cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
+levelplot(TGr, col.regions=cl)
+
+levelplot(TGr,col.regions=cl, names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+
+levelplot(TGr,col.regions=cl, main="LST variation in time",
+          names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+
+# Melt
+meltlist <- list.files(pattern="melt")
+melt_import <- lapply(meltlist,raster)
+melt <- stack(melt_import)
+melt
+
+levelplot(melt)
+
+melt_amount <- melt$X2007annual_melt - melt$X1979annual_melt
+
+clb <- colorRampPalette(c("blue","white","red"))(100)
+plot(melt_amount, col=clb)
+
+levelplot(melt_amount, col.regions=clb)
