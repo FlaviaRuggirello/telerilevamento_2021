845c33531e85980b37ea78d5f751c20f01e2e225
Flavia Ruggirello <80512151+FlaviaRuggirello@users.noreply.github.com>
2021-04-14 12:28:49 +0200

    Create R_code_copernicus.r

---
+++ R_code_copernicus.r
@@ -0,0 +1,12 @@
+#R_code_copernicus 
+#visualizzazione dati copernicus 
+#ci servono la libreria raster - netCDF4 ( per leggere formato dati scaricati)
+
+library(raster) 
+
+#install.packages("ncdf4") 
+
+install.packages("ncdf4")
+library(ncdf4) 
+
+setwd
