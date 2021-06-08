66c0ebe6d9775fe3bf2cb918403b47fcaf6be7a9
Flavia Ruggirello <80512151+FlaviaRuggirello@users.noreply.github.com>
2021-06-08 14:56:16 +0200

    Update 4R_code_knitr.r

--- 4R_code_knitr.r
+++ 4R_code_knitr.r
@@ -14,12 +14,15 @@ library(knitr)
 #usiamo un gestore di testo dove copieremo tutto il codice che vogliamo importare
 #mettiamo all'interno della nostra cartella tutto il necessario (sia i file come immagini che il codice)
 
-setwd("c:/lab/greenland1") #questo nel nostro caso ma bisogna selezionare la cartella corrispondente
+setwd("c:/lab/greenland1") 
+
+#questo nel nostro caso ma bisogna selezionare la cartella corrispondente
 getwd() #per controllare la cartella di lavoro
 
 #cosa stiamo facendo in questa maniera? stiamo importando dalla cartella selezionata il file contenente il codice per creare un pdf unico
-#questo pdf avrà al suo interno tutto quello su cui abbiamo lavorato.
+#questo pdf avrà al suo interno tutto quello su cui abbiamo lavorato
+
 #Per fare questo utilizziamo la funzione stitch dove per prima cosa metteremo il nome del file di testo che abbiamo creato
 stitch("greenland.r", template = system.file("misc", "knitr-template.Rnw", package="knitr"))
 
-CHIEDERE A LUDOVICOOOOOO
+#overleaf- 
