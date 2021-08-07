663e22366d585eadbe0591fd0cb7c7b990df00eb
Flavia Ruggirello <80512151+FlaviaRuggirello@users.noreply.github.com>
2021-08-07 10:21:42 +0200

    Update 7R_code_vegetation_indices.r

--- 7R_code_vegetation_indices.r
+++ 7R_code_vegetation_indices.r
@@ -1,129 +1,88 @@
 # R_code_vegetation_indices.r
 
 library(raster) # require(raster)
-library(RStoolbox) # for vegetation indices calculation 
+library(RStoolbox) # per il calcolo degli indici di vegetazione
 
-setwd("~/lab/") # Linux
+# install.packages("rasterdiv")
 
-# setwd("Desktop/lab/") # Mac
+# per l'indice NDVI del pianeta
+library(rasterVis)
 
-defor1 <- brick("defor1.jpg")
-defor2 <- brick("defor2.jpg")
+setwd("C:/lab/")
 
-# b1 = NIR, b2 = red, b3 = green
+defor1<-brick("defor1.jpg")
+defor2<-brick("defor2.jpg")
+
+# b1= NIR, b2= red, b3= green
 
 par(mfrow=c(2,1))
 plotRGB(defor1, r=1, g=2, b=3, stretch="lin")
-plotRGB(defor2, r=1, g=2, b=3, stretch="lin")
+plotRGB(defor2, r=1, g=2, b=3, stretch="lin") # vediamo le due immagine sulla stessa colonna, si nota la grande diminuzione di vegetazione della foresta amazzonica
 
-defor1
+# calcoliamo il primo indice di vegetazione
 
-# difference vegetation index
+defor1 # andiamo a vedere il nome delle bande aprendo tutte le informazioni del file e vediamo che il NIR si chiama defor1.1 e il RED defor1.2
 
-# time 1
-dvi1 <- defor1$defor1.1 - defor1$defor1.2
+dvi1 <- defoder1$defor1.1-defor1$defor1.2 # per ogni pixel prendiamo il valore nel NIR e sottraiamo il valore nel RED, si crea una mappa composta dalla differenza dei pixel = DVI
 
-# dev.off()
-plot(dvi1)
+plotdvi1
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifichiamo lo schema dei colori in modo che si veda bene la vegetazione in rosso
 
-cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
+plot(dvi1, col=cl, main="DVI at time 1") # in questo modo visualizziamo il DVI che abbiamo appena calcolato
 
+# calcoliamo l'indice di vegetazione per la seconda immagine
+dvi2 <- defor2$defor2.1-defor2$defor2.2
+
+plotdvi2
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100)
+
+plot(dvi2, col=cl, main="DVI at time 2") # è ben visibile in giallo la parte in cui non è più presente vegetazione
+
+# mettiamo le due immagini a confronto
+par (mfrow=c(2,1))
 plot(dvi1, col=cl, main="DVI at time 1")
-
-# time 2
-dvi2 <- defor2$defor2.1 - defor2$defor2.2
-
 plot(dvi2, col=cl, main="DVI at time 2")
 
-par(mfrow=c(2,1))
-plot(dvi1, col=cl, main="DVI at time 1")
-plot(dvi2, col=cl, main="DVI at time 2")
+# Qual'è la differenza nella stessa zona tra questi due tempi? facciamo la differenza tra i DVI delle due situazioni
+difdvi <- dvi1 - dvi2 #facciamo la differenza per ogni pixel, anche se hanno estensione diversa viene calcolata la differenza dove è presente l'intersezione.
 
-difdvi <- dvi1 - dvi2
+plot(difdvi)
 
-# dev.off()
-cld <- colorRampPalette(c('blue','white','red'))(100) 
+cld <- colorRampPalette(c('blue','white','red'))(100)
 plot(difdvi, col=cld)
 
+# ndvi: facciamo la standardizzazione sulla somma dell'indice di vegetazione
+# NDVI= (NIR-RED) / (NIR+RED)
 
-# ndvi
-# (NIR-RED) / (NIR+RED)
 ndvi1 <- (defor1$defor1.1 - defor1$defor1.2) / (defor1$defor1.1 + defor1$defor1.2)
-plot(ndvi1, col=cl)
+# si potrebbe anche fare ndvi1 <- dvi1 / (defor1$defor1.1 + defor1$defor1.2)
 
-# ndvi1 <- dvi1 / (defor1$defor1.1 + defor1$defor1.2)
-# plot(ndvi1, col=cl)
+
+plot(ndvi1)
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100)
+
+plot(ndvi1, col=cl)
 
 ndvi2 <- (defor2$defor2.1 - defor2$defor2.2) / (defor2$defor2.1 + defor2$defor2.2)
 plot(ndvi2, col=cl)
 
-# ndvi1 <- dvi2 / (defor2$defor2.1 + defor1$defor2.2)
-# plot(ndvi2, col=cl)
-
-difndvi <- ndvi1 - ndvi2
-
-# dev.off()
-cld <- colorRampPalette(c('blue','white','red'))(100) 
-plot(difndvi, col=cld)
-
-
-# RStoolbox::spectralIndices
+# Rstoolbox: spectralIndices
+# si può usare la funzione spectralIndices che calcola diversi indici multrispettrali
 vi1 <- spectralIndices(defor1, green = 3, red = 2, nir = 1)
-plot(vi1, col=cl)
+plot (vi1, col=cl)
 
 vi2 <- spectralIndices(defor2, green = 3, red = 2, nir = 1)
-plot(vi2, col=cl)
+plot (vi2, col=cl)
 
-#5.05.2021
-#instal.packages(rastervid) - installiamo il pacchetto che serve per caricare --> troviamo un dataset gratuito (long term dataset )
-# library(rasterdiv) for the Worldwide NDVI
-#COS'è ndvi : 
+difndvi <- ndvi1 - ndvi2 # vediamo la differenza tra gli indici di vegetazione delle due immagini normalizzati
+cld <- colorRampPalette(c('blue','white','red'))(100)
+plot(difdvi, col=cld)
 
+# worldwilde NDVI
 plot(copNDVI)
 
-#plottiamo semplicemente il pacchetto e compare mappa con H2O
-
-copNDVI <- reclassify(copNDVI, cbind(253:255, NA))
+copNDVI <- reclassify(copNDVI, cbind(253:255, NA)) # per oscurare i valori dell'acqua che non ci interessano
 plot(copNDVI)
 
-library(rasterVis)
-
-# ndvi
-# (NIR-RED) / (NIR+RED)
-ndvi1 <- (defor1$defor1.1 - defor1$defor1.2) / (defor1$defor1.1 + defor1$defor1.2)
-plot(ndvi1, col=cl)
-
-# ndvi1 <- dvi1 / (defor1$defor1.1 + defor1$defor1.2)
-# plot(ndvi1, col=cl)
-
-ndvi2 <- (defor2$defor2.1 - defor2$defor2.2) / (defor2$defor2.1 + defor2$defor2.2)
-plot(ndvi2, col=cl)
-
-# ndvi1 <- dvi2 / (defor2$defor2.1 + defor1$defor2.2)
-# plot(ndvi2, col=cl)
-
-difndvi <- ndvi1 - ndvi2
-
-# dev.off()
-cld <- colorRampPalette(c('blue','white','red'))(100) 
-plot(difndvi, col=cld)
-
-
-# RStoolbox::spectralIndices
-vi1 <- spectralIndices(defor1, green = 3, red = 2, nir = 1)
-plot(vi1, col=cl)
-
-vi2 <- spectralIndices(defor2, green = 3, red = 2, nir = 1)
-plot(vi2, col=cl)
-
-# worldwide NDVI
-plot(copNDVI)
-
-
-# Pixels with values 253, 254 and 255 (water) will be set as NA’s.
-copNDVI <- reclassify(copNDVI, cbind(253:255, NA))
-plot(copNDVI)
-
-# rasterVis package needed:
-levelplot(copNDVI)
-
+# è necessario richiamare il pacchetto rasterVis
+levelplot(copNDVI) # immagine che mostra i valori di biomassa della Terra
